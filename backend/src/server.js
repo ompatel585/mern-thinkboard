fcaf0350439ed474c34a595109a17a528ee374f6
ompatel585 <84760165+ompatel585@users.noreply.github.com>
2025-06-19 23:18:10 +0530

    Deployment changes

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,39 +1,47 @@
-import express from 'express';
-import notesRoutes from './routes/notesRoutes.js';
-import { connectDB } from './config/db.js';
-import dotenv from 'dotenv';
-import rateLimiter from './middleware/rateLimiter.js';
-import cors from 'cors'
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import path from "path";
+
+import notesRoutes from "./routes/notesRoutes.js";
+import { connectDB } from "./config/db.js";
+import rateLimiter from "./middleware/rateLimiter.js";
+
 dotenv.config();
 
-const PORT = process.env.PORT || 5001
-
 const app = express();
+const PORT = process.env.PORT || 5001;
+const __dirname = path.resolve();
 
-//middleware
-
-app.use(
-    cors({
-        origin: "http://localhost:5173"
-    })
-);
-
-app.use(express.json());
+// middleware
+if (process.env.NODE_ENV !== "production") {
+    app.use(
+        cors({
+            origin: "http://localhost:5173",
+        })
+    );
+}
+app.use(express.json()); // this middleware will parse JSON bodies: req.body
 app.use(rateLimiter);
 
-//app.use(express.urlencoded({ extended: true }));
+// our simple custom middleware
+// app.use((req, res, next) => {
+//   console.log(`Req method is ${req.method} & Req URL is ${req.url}`);
+//   next();
+// });
 
-app.use((req,res,next) =>{
-    console.log(`Requested method is ${req.method} & Requested URL is ${req.url}`);
-    next();
-})
+app.use("/api/notes", notesRoutes);
 
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-app.use('/api/notes', notesRoutes);
-
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+    });
+}
 
 connectDB().then(() => {
     app.listen(PORT, () => {
-        console.log("Server started on PORT :", PORT);
+        console.log("Server started on PORT:", PORT);
     });
-})
+});
